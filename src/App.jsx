346daebf2d6518584e75c131bc07ff1bad3cee05
Tharindu shakya <73313576+tharindushakya@users.noreply.github.com>
2023-09-01 08:27:18 +0000

    done main portfolio details and .json file deploye

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Tharindu Shakya",
+  title: "Cyber Security Undergraduate",
+  email: "tharindu.github@gmail.com",
+  gitHub: "https://github.com/tharindushakya",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/tharindu-shakya-93775318b/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/wtshakya",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
