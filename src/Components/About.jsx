7d9aabd1e8c6af0aa1c82f401875dbd1299d3771
Tharindu shakya <73313576+tharindushakya@users.noreply.github.com>
2025-02-21 15:58:08 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+//import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -31,12 +31,7 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  <iframe src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=303130" style='border:none;'></iframe>
 ];
 
 /**
